c168993906fcbf933e5867c32f1fea6ccaba5713
Aya Elsayed <45416886+ayaelsayed25@users.noreply.github.com>
2020-12-18 09:52:45 +0200

    hi

--- src/main/java/com/example/mail_server/Controller/controller.java
+++ src/main/java/com/example/mail_server/Controller/controller.java
@@ -2,13 +2,29 @@ package com.example.mail_server.Controller;
 
 import com.example.mail_server.Model.Mail;
 import com.example.mail_server.Model.User;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
+@Deprecated
+class MyConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedHeaders("*");
+            }
+        };
+    }
+
+}
 @RestController
-
+@CrossOrigin
 public class controller {
     private User user;
 
@@ -21,5 +37,11 @@ public class controller {
         user.Compose(mail);
     }
 
-
+    @CrossOrigin
+    @RequestMapping("/createAccount")
+    @ResponseBody
+    public boolean createAccount(@RequestParam (value = "name") String name, @RequestParam(value = "email") String email, @RequestParam(value = "password") String password)
+    {
+        return true;
+    }
 }
